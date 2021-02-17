3d45d76045944f093cf55e0cc1770eb287b173a3
leeseooo <leeseoooo@naver.com>
2021-02-17 22:00:33 +0900

    create carousel_slider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/model/model_movie.dart';
+import 'package:flutter_app/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -9,17 +10,42 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
     Movie.fromMap({
       'title': '브리저튼',
-      'keyword': '사랑/로맨스/19금',
+      'keyword': '로맨스/시대물',
       'poster': 'bridgerton.jpg',
       'like': false
-    })
+    }),
+    Movie.fromMap({
+      'title': '오펀 블랙',
+      'keyword': 'SF/드라마/액션',
+      'poster': 'orphanblack.jpg',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '러브 앤 아나키',
+      'keyword': '로맨스/코미디',
+      'poster': 'loveandanarchy.jpeg',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '기묘한 이야기',
+      'keyword': 'SF/미스터리',
+      'poster': 'strangethings.png',
+      'like': false
+    }),
   ];
   void initState() {
     super.initState();
   }
 
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: <Widget>[
+        Stack(children: <Widget>[
+          CarouselImage(movies: movies),
+          TopBar(),
+        ])
+      ],
+    );
   }
 }
 
